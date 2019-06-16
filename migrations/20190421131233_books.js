7e8acb4d278f1a7510fcc49ee924362e71bc635f
Conrad Wright <easp.conrad@gmail.com>
2019-06-16 15:52:43 -0600

    Update migration and seed files

--- migrations/20190421131233_books.js
+++ migrations/20190421131233_books.js
@@ -1,19 +1,21 @@
-
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('books', (book) => {
-    book.increments('id')
-    book.string('title')
-    book.string('author')
-    book.string('genre')
-    book.integer('pages')
-    book.integer('year')
-    book.string('imageLink')
-    book.string('language')
-    book.string('link')
-  })
+  return knex.schema.createTable("books", book => {
+    book.increments("id");
+    book.string("author");
+    book.text("bookSummary");
+    book.string("genre");
+    book.boolean("haveRead");
+    book.string("imageLink");
+    book.string("language");
+    book.string("link");
+    book.integer("pages");
+    book.boolean("recommendable");
+    book.string("subtitle");
+    book.string("title");
+    book.integer("year");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('books')
-
+  return knex.schema.dropTableIfExists("books");
 };
--- seeds/01_books.js
+++ seeds/01_books.js
@@ -1,22 +1,26 @@
-
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('books').del()
-    .then(function () {
+  return knex("books")
+    .del()
+    .then(function() {
       // Inserts seed entries
-      return knex('books').insert(
-        [
-          {
-            title: "Things Fall Apart",
-            author: "Chinua Achebe",
-            genre: "Historical Memoir",
-            pages: 209,
-            year: 1958,
-            imageLink: "images/things-fall-apart.jpg",
-            language: "English",
-            link: "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
-          },
-        ]
-      );
+      return knex("books").insert([
+        {
+          author: "Jordan Peterson",
+          bookSummary:
+            "In-depth look at human consciousness through the lense of the Yin/Yang dichotomy",
+          genre: "Psychology/Self Improvement",
+          haveRead: false,
+          imageLink:
+            "https://dynamic.indigoimages.ca/books/0345816021.jpg?altimages=false&scaleup=true&maxheight=515&width=380&quality=85&sale=14&lang=fr",
+          language: "English",
+          link: "https://www.jordanbpeterson.com/12-rules-for-life/",
+          pages: 315,
+          recommendable: true,
+          subtitle: "Antidote to Chaos",
+          title: "12 Rules For Life",
+          year: 2017
+        }
+      ]);
     });
 };
